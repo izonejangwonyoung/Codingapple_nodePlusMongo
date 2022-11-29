d44f97600f4fe48a5fa4f74ac5df71adb1047936
izonejangwonyoung <2000shim@naver.com>
2022-11-30 01:11:30 +0900

    221129-db 컨텐츠 ejs 이용하여 리스트 페이지에 불러오기 및 반복문으로 모든 데이터 값 자동 출력

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ MongoClient.connect(process.env.mongo_address, function (에러, client) {
     db = client.db('todoapp');   ////tododapp이라는 데이터베이스에 연결
 
 
-    db.collection('post').insertOne({이름: '심우혁', 나이: 23, _id: 77}, function (에러, 결과) {
-        console.log('저장완료');
-    });
+    // db.collection('post').insertOne({이름: '심우혁', 나이: 23, _id: 77}, function (에러, 결과) {
+    //     console.log('저장완료');
+    // });
 
     app.listen(8080, function () {
         console.log('listening on 8080')
@@ -28,6 +28,17 @@ app.post('/add', function (요청, 응답) {
     })
 })
 
-app.get('/write',function (요청,응답){
-    응답.sendFile(__dirname+'/write.html')
+app.get('/write', function (요청, 응답) {
+    응답.sendFile(__dirname + '/write.html')
+})
+
+
+app.get('/list', function (요청, 응답) {
+
+
+    db.collection('post').find().toArray(function (에러, 결과) {
+        console.log(결과);
+        응답.render('list.ejs',{posts: 결과});
+    });///모든 데이터 가져오기 문법
+
 })
