4e5852ee8a87e3b1b30ac144d939b665dc95e6c6
izonejangwonyoung <2000shim@naver.com>
2022-12-02 00:07:45 +0900

    20221201- 디테일 페이지+ edit 기능 구현만. 복습 필수,,

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config();
 app.set('view engine', 'ejs')
 let db;
+const methodOverride=require('method-override')
+app.use(methodOverride('_method'))
 MongoClient.connect(process.env.mongo_address, function (에러, client) {
 
     if (에러) return console.log(에러)//에러처리
@@ -71,10 +73,10 @@ app.get('/list', function (요청, 응답) {
 
 app.delete('/delete', function (요청, 응답) {
     요청.body._id = parseInt(요청.body._id)
-    db.collection('post').deleteOne(요청.body,function (에러,결과){
+    db.collection('post').deleteOne(요청.body, function (에러, 결과) {
         console.log('삭제완료')
     });
-    db.collection('counter').updateOne({name: '게시물갯수'}, {$set: {totalPost: 0}}),function (요청,응답){
+    db.collection('counter').updateOne({name: '게시물갯수'}, {$set: {totalPost: 0}}), function (요청, 응답) {
         console.log("카운트 초기화 완료")
     }, function (에러, 응답) {
         if (에러) {
@@ -83,3 +85,24 @@ app.delete('/delete', function (요청, 응답) {
         응답.send('전체삭제완료')
     }
 })
+
+app.get('/detail/:id', function (요청, 응답) {
+    db.collection('post').findOne({_id: parseInt(요청.params.id)}, function (에러, 결과) {
+        응답.render('detail.ejs', {posts: 결과})
+    })
+});
+
+app.get('/edit/:id', function (req, res) {
+    db.collection('post').findOne({_id: parseInt(req.params.id)}, function (에러, 결과) {
+        res.render('edit.ejs', {edit: 결과})
+    })
+})
+
+app.put('/edit', function(요청, 응답){
+    db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 요청.body.title , 날짜 : 요청.body.date }}, function(){
+
+        console.log('수정완료')
+        응답.redirect('/list')
+
+    });
+});
