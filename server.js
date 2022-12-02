2086550910b7c045ba488959f29c9359dc8e3674
izonejangwonyoung <2000shim@naver.com>
2022-12-03 02:14:11 +0900

    20221203 회원, 로그인 세션 구현, 권한 별 페이지 접속 권한 설정

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config();
 app.set('view engine', 'ejs')
+const crypto = require('crypto');
 let db;
-const methodOverride=require('method-override')
+const methodOverride = require('method-override')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const createHashedPassword = (password) => {
+    return crypto.createHash("sha512").update(password).digest("base64");
+};
+
+app.use(session({secret: '비밀코드', resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(methodOverride('_method'))
 MongoClient.connect(process.env.mongo_address, function (에러, client) {
 
@@ -74,23 +85,26 @@ app.get('/list', function (요청, 응답) {
 app.delete('/delete', function (요청, 응답) {
     요청.body._id = parseInt(요청.body._id)
     db.collection('post').deleteOne(요청.body, function (에러, 결과) {
-        console.log('삭제완료')
-    });
+            console.log('삭제완료')
+            응답.redirect('/list')
+        }
+    );
     db.collection('counter').updateOne({name: '게시물갯수'}, {$set: {totalPost: 0}}), function (요청, 응답) {
         console.log("카운트 초기화 완료")
     }, function (에러, 응답) {
         if (에러) {
             return console.log(에러)
         }
-        응답.send('전체삭제완료')
     }
 })
 
 app.get('/detail/:id', function (요청, 응답) {
-    db.collection('post').findOne({_id: parseInt(요청.params.id)}, function (에러, 결과) {
-        응답.render('detail.ejs', {posts: 결과})
+    db.collection('post').findOne({
+        function(에러, 결과) {
+            응답.render('detail.ejs', {posts: 결과})
+        }
     })
-});
+})
 
 app.get('/edit/:id', function (req, res) {
     db.collection('post').findOne({_id: parseInt(req.params.id)}, function (에러, 결과) {
@@ -98,11 +112,107 @@ app.get('/edit/:id', function (req, res) {
     })
 })
 
-app.put('/edit', function(요청, 응답){
-    db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 요청.body.title , 날짜 : 요청.body.date }}, function(){
+app.put('/edit', function (요청, 응답) {
+    db.collection('post').updateOne({_id: parseInt(요청.body.id)}, {
+        $set: {
+            제목: 요청.body.title,
+            날짜: 요청.body.date
+        }
+    }, function () {
 
         console.log('수정완료')
         응답.redirect('/list')
 
     });
+});
+app.get('/home', function (req, res) {
+    db.collection('counter').findOne(function (error, result) {
+        res.render('home.ejs', {data: result})
+    })
+})
+
+app.get('/login', function (req, res) {
+    res.render('login.ejs')
+})
+app.post('/login', passport.authenticate('local', {failureRedirect: '/fail'}), function (req, res) {
+    res.redirect('/home')
+});
+
+app.get('/mypage', isLogin, function (req, res) {
+    res.render('mypage.ejs', {user: req.user})
+})
+
+function isLogin(req, res, next) {
+    console.log(req.user)
+    if (req.user) {
+        next()
+    } else {
+        res.send('로그인안하셨는데요?')
+    }
+}
+
+
+app.get("/logout", function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/home");
+    });
+});
+// app.get('/logout', function (req, res, next) {
+//     req.logout(function(err) {
+//         if (err) { return next(err); }
+//         res.session.destroy();
+//         res.render('home.ejs');
+//     req.logout();
+//     res.redirect('/home');
+// });
+app.get('/join', function (req, res) {
+    res.render('join.ejs')
+})
+app.post('/join', function (req, res) {
+    // const hashId = crypto.createHash('sha512').update(req.body.id + salt).digest('hex');
+    // const hashPw = crypto.createHash('sha512').update(req.body.pw + salt).digest('hex');
+
+    crypto.pbkdf2(req.body.id, "salt", 65536, 32, "sha512", (err, derivedKey) => {
+        if (err) {
+            throw err;
+        }
+        //에러 발생시 핸들링
+        console.log(derivedKey.toString("hex"));
+    });
+    // let cryptedId=createHashedPassword(req.body.id)
+    db.collection('login').insertOne({id: req.body.id, pw: req.body.pw})
+    res.send('complete')
+})
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({id: 입력한아이디}, function (에러, 결과) {
+        if (에러) return done(에러)
+
+        if (!결과) return done(null, false, {message: '존재하지않는 아이디요'})
+        if (입력한비번 == 결과.pw) {
+            return done(null, 결과)
+        } else {
+            return done(null, false, {message: '비번틀렸어요'})
+        }
+    })
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id)
+});
+
+passport.deserializeUser(function (아이디, done) {
+    db.collection('login').findOne({id: 아이디}, function (에러, 결과) {
+        done(null, 결과)
+    })
 });
