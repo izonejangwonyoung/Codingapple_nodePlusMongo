327188bb8a563fc8f1416b44239e96a17ad2f797
izonejangwonyoung <2000shim@naver.com>
2023-03-05 15:20:27 +0900

    edit main page

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ MongoClient.connect(process.env.MONGO_ADDRESS, function (에러, client) {
 
 
 app.get('/', function(req, res){
-    res.send('빈 페이지입니다. <a href="footprint.ericshim.me/login">로그인 페이지</a>로 가기');
+    res.send('빈 페이지입니다. <a href="/login">로그인 페이지</a>로 가기');
 });
 app.post('/addcomplete', function (요청, 응답) {
     db.collection('counter').findOne({name: '게시물갯수'}, function (에러, 결과) {
